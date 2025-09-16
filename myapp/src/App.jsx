cadbd065a7f60b658bf864371bfb63851a65610f
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-09-16 23:35:36 +0530

    feat: refactor App component and add new components

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,27 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Component from './Component.jsx'
+import { ComponetB, ComponentC } from './Component.jsx'
 
 function App() {
-  const elementA = ()=>{
-    return(
-      <div>
-        <h1>welcome to coding hunger.</h1>
-      </div>
-    )
-  }
-  const elementB = ()=>{
-    return(
-      <div>
-        <h1>hello developer</h1>
-      </div>
-    )
-  }
-let num = 1;
+//   const elementA = ()=>{
+//     return(
+//       <div>
+//         <h1>welcome to coding hunger.</h1>
+//       </div>
+//     )
+//   }
+//   const elementB = ()=>{
+//     return(
+//       <div>
+//         <h1>hello developer</h1>
+//       </div>
+//     )
+//   }
+// let num = 1;
   return (
     <>
-      {num%2===0?elementA():elementB()}
+      {/* {num%2===0?elementA():elementB()} */}
       <h1>welcome guys</h1> 
+      <Component/>
+      <ComponetB/>
+      <ComponentC/>
     </>
   )
 }
---
+++ myapp/src/Component.jsx
@@ -0,0 +1,22 @@
+const Component = () =>{
+    return(
+        <div>
+            <h1>hello from component A.</h1>
+        </div>
+    )
+}
+export const ComponetB = ()=>{
+    return(
+        <div>
+            <h2>hello from component B</h2>
+        </div>
+    )
+}
+export const ComponentC = ()=>{
+    return(
+        <div>
+            <h1>hello from Component C</h1>
+        </div>
+    )
+}
+export default Component; 
