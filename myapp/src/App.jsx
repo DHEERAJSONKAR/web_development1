85c1e86330856c1e3cb32a2be4c61dcad0d35052
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-09-17 11:27:14 +0530

    feat: add Button component and update App component to use it

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Component from './Component.jsx'
+// import Component from './Component.jsx'
 // import { ComponetB, ComponentC } from './Component.jsx'
+import Button  from './Button'  
 
 function App() {
 //   const elementA = ()=>{
@@ -21,18 +22,21 @@ function App() {
 //     )
 //   }
 // let num = 1;
-let username = "coding hunger"  
+// let username = "coding hunger"
+const handleOnClickButton = (name) =>{
+  alert(`hello coder, ${name}`)
+}  
 return (
     <>
       {/* {num%2===0?elementA():elementB()} */}
       {/* <h1>welcome guys</h1>  */}
-      
-      <Component username={"coding lover"} roll = {"24mca045"}/>
-      <Component username={"coding enthusiast"} roll = {"24mca045"}/>
-      <Component username={"coding hunger"} roll = {"24mca045"}/>
-      {/* <ComponetB/>
+      {/* <Component username={"coding lover"} roll = {"24mca045"}/> */}
+      {/* <Component username={"coding enthusiast"} roll = {"24mca045"}/> */}
+      {/* <Component username={"coding hunger"} roll = {"24mca045"}/> */}
+      {/* /* <ComponetB/> */}
+      <Button handleonclickfunction = {handleOnClickButton} text={"click me"}/>
 
-      <ComponentC/> */}
+      {/* <ComponentC/> */ }
     </>
   )
 }
---
+++ myapp/src/Button.jsx
@@ -0,0 +1,11 @@
+const Button = ({text, handleonclickfunction}) =>{
+    const handlebutton = () =>{
+        handleonclickfunction("youtube channel.");
+    }
+    return(
+        <div>
+            <button onClick= {handlebutton}>{text}</button>
+        </div>
+    )
+}
+export default Button;
