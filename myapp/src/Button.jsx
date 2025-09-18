49b044186847f2ceaaebfa921ba0b26872f0747c
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-09-18 17:30:42 +0530

    refactor: comment out Button component code for future reference

--- myapp/src/Button.jsx
+++ myapp/src/Button.jsx
@@ -1,11 +1,14 @@
-const Button = ({text, handleonclickfunction}) =>{
-    const handlebutton = () =>{
-        handleonclickfunction("youtube channel.");
-    }
-    return(
-        <div>
-            <button onClick= {handlebutton}>{text}</button>
-        </div>
-    )
-}
-export default Button;
+// const Button = ({text, handleonclickfunction}) =>{
+
+//     const handlebutton = () =>{
+//         handleonclickfunction("youtube channel.");
+//     }
+//     return(
+//         <div>
+//             <button onClick= {handlebutton}>{text}</button>
+//         </div>
+//     )
+// }
+// export default Button;
+
+
