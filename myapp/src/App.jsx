4b538c96ed2ccc7c9e9fca4e83b7d934b12c8aad
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-10-03 10:19:43 +0530

    refactor: remove Button component and update Component to export additional components

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,42 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
+ 
 import './App.css'
-// import Component from './Component.jsx'
-// import { ComponetB, ComponentC } from './Component.jsx'
-import Button  from './Button'  
+import Component from './Component'
+
+
 
 function App() {
-//   const elementA = ()=>{
-//     return(
-//       <div>
-//         <h1>welcome to coding hunger.</h1>
-//       </div>
-//     )
-//   }
-//   const elementB = ()=>{
-//     return(
-//       <div>
-//         <h1>hello developer</h1>
-//       </div>
-//     )
-//   }
-// let num = 1;
-// let username = "coding hunger"
-const handleOnClickButton = (name) =>{
-  alert(`hello coder, ${name}`)
-}  
+ 
 return (
     <>
-      {/* {num%2===0?elementA():elementB()} */}
-      {/* <h1>welcome guys</h1>  */}
-      {/* <Component username={"coding lover"} roll = {"24mca045"}/> */}
-      {/* <Component username={"coding enthusiast"} roll = {"24mca045"}/> */}
-      {/* <Component username={"coding hunger"} roll = {"24mca045"}/> */}
-      {/* /* <ComponetB/> */}
-      <Button handleonclickfunction = {handleOnClickButton} text={"click me"}/>
-
-      {/* <ComponentC/> */ }
+     
+ <Component/>
+      
     </>
   )
 }
--- myapp/src/Component.jsx
+++ myapp/src/Component.jsx
@@ -5,18 +5,18 @@ const Component = ({username, roll}) =>{
         </div>
     )
 }
-// export const ComponetB = ()=>{
-//     return(
-//         <div>
-//             <h2>hello from component B</h2>
-//         </div>
-//     )
-// }
-// export const ComponentC = ()=>{
-//     return(
-//         <div>
-//             <h1>hello from Component C</h1>
-//         </div>
-//     )
-// }
+export const ComponetB = ()=>{
+    return(
+        <div>
+            <h2>hello from component B</h2>
+        </div>
+    )
+}
+export const ComponentC = ()=>{
+    return(
+        <div>
+            <h1>hello from Component C</h1>
+        </div>
+    )
+}
 export default Component; 
